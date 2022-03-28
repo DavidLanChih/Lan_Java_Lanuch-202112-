d9ec1fd8af67ad71e112370a21c064460e6ab087
DavidLanChih <cljh20220@gmail.com>
2022-03-29 00:00:51 +0800

    home

---
+++ Java_LAN_test/src/test/replace.java
@@ -0,0 +1,9 @@
+package test;
+
+public class replace {
+	public static void main(String args[]){
+		String A= "show";
+		System.out.println(A.replace('s', 'a'));
+		System.out.println(A.getClass());
+	}
+}
